248c2a3b1ba6e01a4997322e5459eb52540b22a2
Irina Berestova <berestovaecdep@mail.ru>
2022-11-10 00:09:26 +0700

    устранить ошибку авторизации payload

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,8 @@ const ConflictErr = require('../errors/conflict-err');
 const UnauthorizedErr = require('../errors/unauthorized-err');
 const User = require('../models/user');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports.getUserMe = (req, res, next) => {
   User.findById(req.user._id)
     .then((user) => {
@@ -72,7 +74,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      res.send({ token: jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' }) });
+      res.send({ token: jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' }) });
     })
     .catch(() => {
       next(new UnauthorizedErr('Неправильные почта или пароль'));
