7666ac420a86f9574052ad3b584fb7175d2737f9
Irina Berestova <berestovaecdep@mail.ru>
2022-11-11 23:46:54 +0700

    убрать лишние строки в app.js

--- app.js
+++ app.js
@@ -12,24 +12,24 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
 const { NODE_ENV, DB_URL, PORT = 3000 } = process.env;
-// const { DB_URL = 'mongodb://localhost:27017/moviesdb', PORT = 3000 } = process.env;
+
 const app = express();
 app.use(cors);
 
-app.use(bodyParser.json()); // для собирания JSON-формата
-app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(NODE_ENV === 'production' ? DB_URL : devDataBaseURL);
 
-app.use(requestLogger); // подключаем логгер запросов
+app.use(requestLogger);
 app.use(helmet());
 app.use(limiter);
 
 app.use(router);
 
-app.use(errorLogger); // подключаем логгер ошибок
-// обработчики ошибок
-app.use(errors()); // обработчик ошибок celebrate
-app.use(handlerErrors); // централизованнный обработчик
+app.use(errorLogger);
+
+app.use(errors());
+app.use(handlerErrors);
 
 app.listen(PORT);
