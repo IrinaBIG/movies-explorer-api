4f1b40d1a49a8f4ab09a4edd9dff4584db3629f7
Irina Berestova <berestovaecdep@mail.ru>
2023-02-05 11:44:04 +0700

    прилож cors mid

--- app.js
+++ app.js
@@ -4,29 +4,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const cors = require('cors');
 const { limiter, devDataBaseURL } = require('./utils/config');
 const router = require('./routes');
 const handlerErrors = require('./middlewares/handlerErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const allowedCors = [
-  'https://api.diplomabig.students.nomoredomains.icu',
-  'https://irinabigdiploma.nomoredomainsclub.ru/',
-  'http://api.diplomabig.students.nomoredomains.icu',
-  'http://irinabigdiploma.nomoredomainsclub.ru/',
-  'http://localhost:7777',
-  'http://localhost:3000',
-  'http://127.0.0.1',
-];
-
-// const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 
 const { NODE_ENV, DB_URL, PORT = 5555 } = process.env;
 
 const app = express();
-// app.use(cors);
-app.use(cors({ origin: allowedCors }));
+app.use(cors);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
